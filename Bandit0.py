9ca05c225fc79c00d83cc80ef1b97afdfbf7d548
cmc330 <cmc330@gmail.com>
2023-05-17 10:13:03 -0400

    solved for bandit0

--- Bandit0.py
+++ Bandit0.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 from pwn import *
 
 session = ssh('bandit0', 'bandit.labs.overthewire.org', password='bandit0', port=2220)
 
 io = session.process('sh')
-io.interactive()
+io.sendline(b'cat readme')
+print(io.recvline())
